ac106849041d8dcef04434616fe5afca21fa00c7
ludovicjj <jahanlud@gmail.com>
2019-09-02 20:57:27 +0200

    Update HomeController, return response and render home template

--- src/Controller/Home/HomeController.php
+++ src/Controller/Home/HomeController.php
@@ -2,15 +2,37 @@
 
 namespace App\Controller\Home;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class HomeController
 {
+    /** @var Environment */
+    private $twig;
+
+    public function __construct(
+        Environment $twig
+    ) {
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/", name="home")
+     *
+     * @return Response
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
      */
     public function home()
     {
-
+        return new Response(
+            $this->twig->render(
+                'home/home.html.twig',
+                []
+            )
+        );
     }
 }
