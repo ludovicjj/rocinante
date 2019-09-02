5a1e2437efc45276a6e64c3fe6d02a64032054fc
ludovicjj <jahanlud@gmail.com>
2019-09-02 20:32:04 +0200

    Init HomeController

---
+++ src/Controller/Home/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Home;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+
+    }
+}
