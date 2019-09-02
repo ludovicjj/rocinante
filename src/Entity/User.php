2d2f8d218d8d019a764c57c7186c758e8e3b5e7b
ludovicjj <jahanlud@gmail.com>
2019-09-02 23:25:56 +0200

    User Entity implements UserInterface & add properties roles, status

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,8 +3,13 @@
 namespace App\Entity;
 
 
-class User
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements UserInterface
 {
+    const STATUS_PENDING_VALIDATION = "pending_activation";
+    const STATUS_ENABLE = "enable";
+
     /** @var string */
     private $username;
 
@@ -14,14 +19,23 @@ class User
     /** @var string */
     private $email;
 
+    /** @var array */
+    private $roles;
+
+    /** @var string */
+    private $status;
+
     public function __construct(
         string $username,
         string $password,
-        string $email
+        string $email,
+        string $roles
     ) {
         $this->username = $username;
         $this->password = $password;
         $this->email = $email;
+        $this->roles[] = $roles;
+        $this->status = self::STATUS_PENDING_VALIDATION;
     }
 
     /**
@@ -46,5 +60,38 @@ class User
     public function getEmail()
     {
         return $this->email;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function enable()
+    {
+        $this->status = self::STATUS_ENABLE;
     }
 }
