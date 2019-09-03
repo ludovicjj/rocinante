45e31772704e780c1cf77140cc51840a009dc19b
ludovicjj <jahanlud@gmail.com>
2019-09-03 14:58:04 +0200

    Add constraints type annotation to CreateUserDTO

--- src/DTO/CreateUserDTO.php
+++ src/DTO/CreateUserDTO.php
@@ -2,20 +2,41 @@
 
 namespace App\DTO;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class CreateUserDTO
 {
     /**
      * @var string|null
+     *
+     * @Assert\NotBlank(message="Vous devez saisir un nom pseudo.")
+     * @Assert\Length(
+     *     max=30,
+     *     maxMessage="Votre pseudo ne peut pas excéder {{ limit }} caractéres."
+     * )
      */
     public $username;
 
     /**
      * @var string|null
+     *
+     * @Assert\NotBlank(message="Vous devez saisir un mot de passe.")
+     * @Assert\Length(
+     *     max=50,
+     *     maxMessage="Votre mot de passe ne peut pas excéder {{ limit }} caractéres."
+     * )
      */
     public $password;
 
     /**
      * @var string|null
+     *
+     * @Assert\NotBlank(message="Vous devez saisir une adresse email.")
+     * @Assert\Length(
+     *     max=60,
+     *     maxMessage="Votre adresse email ne peut pas excéder {{ limit }} caractéres."
+     * )
+     * @Assert\Email(message="Le format de l'adresse email n'est pas correcte.")
      */
     public $email;
 
--- src/Form/CreateUserType.php
+++ src/Form/CreateUserType.php
@@ -2,7 +2,6 @@
 
 namespace App\Form;
 
-
 use App\DTO\CreateUserDTO;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
