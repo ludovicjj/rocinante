b904c59845691510f3c4d4790e2f3939e9060410
ludovicjj <jahanlud@gmail.com>
2019-09-03 14:48:42 +0200

    Create form CreateUserType

---
+++ src/Form/CreateUserType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+
+use App\DTO\CreateUserDTO;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class,
+                [
+                    'label' => "Pseudo",
+                ]
+            )
+            ->add('password', RepeatedType::class,
+                [
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'Les mots de passe ne sont pas identiques',
+                    'required' => true,
+                    'first_options'  => ['label' => 'Mot de passe'],
+                    'second_options' => ['label' => 'Confirmer mot de passe'],
+                ]
+            )
+            ->add('email', TextType::class,
+                [
+                    'label' => 'Email',
+                ]
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => CreateUserDTO::class,
+            'empty_data' => function (FormInterface $form) {
+                return new CreateUserDTO(
+                    $form->get('username')->getData(),
+                    $form->get('password')->getData(),
+                    $form->get('email')->getData()
+                );
+            }
+        ]);
+    }
+}
