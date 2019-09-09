ed96e8a0d5aed957db595a36d72f3e23b2a698eb
ludovicjj <jahanlud@gmail.com>
2019-09-09 18:48:49 +0200

    Update CreateUserController, return RedirectResponse to home page if form is submitted & valid

--- src/Controller/User/CreateUserController.php
+++ src/Controller/User/CreateUserController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\User;
 
 use App\Form\CreateUserType;
+use App\Handler\CreateUserHandler;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Environment;
 
 class CreateUserController
@@ -17,28 +20,44 @@ class CreateUserController
     /** @var FormFactoryInterface */
     private $FormFactory;
 
+    /** @var CreateUserHandler */
+    private $createUserHandler;
+
+    /** @var UrlGeneratorInterface */
+    private $urlGenerator;
+
     public function __construct(
         FormFactoryInterface $formFactory,
-        Environment $twig
+        Environment $twig,
+        CreateUserHandler $createUserHandler,
+        UrlGeneratorInterface $urlGenerator
     ) {
         $this->FormFactory = $formFactory;
         $this->twig = $twig;
+        $this->createUserHandler = $createUserHandler;
+        $this->urlGenerator = $urlGenerator;
     }
 
     /**
      * @Route("/registration", name="create_user")
      *
      * @param Request $request
-     *
-     * @return Response
+     * @return RedirectResponse|Response
      *
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
+     * @throws \Exception
      */
     public function create(Request $request)
     {
         $form = $this->FormFactory->create(CreateUserType::class)->handleRequest($request);
+
+        if ($this->createUserHandler->handle($form)) {
+            return new RedirectResponse(
+                $this->urlGenerator->generate('home')
+            );
+        }
 
         return new Response(
             $this->twig->render(
